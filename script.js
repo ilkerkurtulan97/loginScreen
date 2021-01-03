7a911363e4ec4e256229072f7a0bf040f8fa54c1
ilkerkurtulan97 <45030821+ilkerkurtulan97@users.noreply.github.com>
2021-01-04 00:39:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", function() {
+  toastr.info("Message will come here");
+  console.log("asdasdas");
+});
